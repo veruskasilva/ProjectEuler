e4723693d702fa80de692ebbff4dc1411a471d25
Veruska Rodrigues da Silva <veruskasilva@gmail.com>
2017-12-07 18:12:58 -0200

    primeira versão

---
+++ ProjectEulerCS/ProjectEulerCS/Classes/Euler001.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerCS.Classes
+{
+    public class Euler001
+    {
+        //Problem 1
+        //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        //Find the sum of all the multiples of 3 or 5 below 1000.
+        public static int Problem1()
+        {
+            int soma = 0;
+            for(int contador = 1; contador < 1000; contador++)
+            {
+                if ((contador % 3) == 0 || (contador % 5) == 0)
+                {
+                    soma += contador;
+                }
+
+            }
+
+            return soma;
+        }
+    }
+}
---
+++ ProjectEulerCS/ProjectEulerCS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectEulerCS.Classes;
+
+namespace ProjectEulerCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Euler001.Problem1());            
+        }
+    }
+}
